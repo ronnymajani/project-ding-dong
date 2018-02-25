1744b79cd3f53ce3fca5e7ebf82281eb0b5e1a36
Ronny Majani <ronnymajani@gmail.com>
2018-02-25 22:39:24 +0300

    Added more constants

--- code/constants.py
+++ code/constants.py
@@ -1,5 +1,15 @@
 INT_PIN = 14
-
-DOORBELL_MESSAGE = "Knock Knock"
 PIN_LED_STATUS = 5
 PIN_LED_ERROR = 4
+
+DOORBELL_MESSAGE = "Knock Knock"
+DOORBELL_AGAIN_MESSAGE = "Knock Knock Again"
+
+
+WIFI_TIMEOUT = 60
+# Time to keep the system on before going into deepsleep when an error occurrs
+ERROR_PRE_SLEEP_TIME = 5
+# Time to sleep before restarting the system upon an unkown error
+ERROR_SLEEP_TIME = 60
+# Time to sleep before restarting the system when the device fails to connect to the wifi
+WIFI_ERROR_SLEEP_TIME = 1800  # 30 minutes
