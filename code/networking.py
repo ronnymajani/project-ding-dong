9e34c69f3ee49abee3897d13e8534101933139dd
Ronny Majani <ronnymajani@gmail.com>
2018-02-25 12:14:41 +0300

    Implemented timeout functionality for connect function

--- code/networking.py
+++ code/networking.py
@@ -1,4 +1,5 @@
 import network
+import time
 
 class NetworkManager(object):
     _sta_if = network.WLAN(network.STA_IF)
@@ -15,10 +16,13 @@ class NetworkManager(object):
         """Returnes True if the device is already connected"""
         return NetworkManager._sta_if.isconnected()
     
-    # TODO: add timeout for conecting to network
     @staticmethod
-    def connect(ssid, password):
-        """Connect to the wifi with the given ssid and password"""
+    def connect(ssid, password, timeout=5):
+        """Connect to the wifi with the given ssid and password
+        @param ssid: The SSID of the network to connect to
+        @param password: The password of the network to connect to
+        @param timeout: Optional parameters specifying the maximum seconds to wait before giving up (must be an integer)
+        """
         st_if = NetworkManager._sta_if
         # Make sure the Station interface is active
         if not st_if.active():
@@ -27,9 +31,16 @@ class NetworkManager(object):
         if not NetworkManager.isconnected():
             st_if.connect(ssid, password)
             # wait for connection to succeed
-            while not st_if.isconnected():
+            deadline = time.time() + timeout
+            while not st_if.isconnected() and time.time() < deadline:
                 pass
-            print("[info] connected to network (%s):" % ssid, st_if.ifconfig())
+            if st_if.isconnected():
+                print("[info] connected to network (%s):" % ssid, st_if.ifconfig())
+                return True
+            else:
+                print("[info] timeout while tying to connect to network (%s)" % ssid)
+                st_if.disconnect()
+                return False
         else:
             print("[info] already connected to network:", st_if.ifconfig())
             
